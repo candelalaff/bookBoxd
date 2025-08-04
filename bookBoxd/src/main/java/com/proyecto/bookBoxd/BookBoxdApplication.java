76461b213c97c48afa7848c35b0a4868e5813276
Cande <134436865+c4ndurela@users.noreply.github.com>
2025-08-03 21:14:23 -0300

    primer commit del proyecto

---
+++ bookBoxd/src/main/java/com/proyecto/bookBoxd/BookBoxdApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.bookBoxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBoxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBoxdApplication.class, args);
+	}
+
+}
---
+++ bookBoxd/src/test/java/com/proyecto/bookBoxd/BookBoxdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.bookBoxd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBoxdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
